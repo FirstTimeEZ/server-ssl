3b95896597f0c5f356f4782234e54153404fc13a
FirstTimeEZ <179362031+FirstTimeEZ@users.noreply.github.com>
2024-11-19 23:30:01 +1300

    server-ssl.js

---
+++ README.md
@@ -0,0 +1,85 @@
+# server-ssl.js
+
+The server itself is included with [`Node.js`](https://nodejs.org/en), so you should install the most recent version.
+
+### Create SSL Certificate
+
+If you don't have a `certificate` and `private key` in `pem format` and are developing locally you can create one
+
+Right click and select [`Open Git Bash Here`](https://git-scm.com/downloads/win) in the same folder as `server-ssl.js`
+
+Run this command with whatever options you need
+
+```
+openssl req -x509 -newkey rsa:2048 -nodes -sha256 -keyout private-key.pem -out certificate.pem -days 365 -subj "//CN=localhost"
+```
+
+### Options
+
+| Option       | Description                                      | Default Value         |
+|--------------|--------------------------------------------------|-----------------------|
+| `--port=`    | Specifies the port number | `3000` |
+| `--cert=`    | Specifies the path to the SSL certificate file. | `"certificate.pem"` |
+| `--pk=`      | Specifies the path to the private key file associated with the SSL certificate. | `"private-key.pem"` |
+| `--root=`    | Specifies the root directory for the website files. | `"website"` |
+
+### Start the Server
+
+Right click and select [`Open Git Bash Here`](https://git-scm.com/downloads/win) in the same folder as `server-ssl.js`
+
+Run this command with whatever options you need
+
+```
+node server-ssl.js --port=443 --cert="certificate.pem" --pk="private-key.pem"
+```
+
+![](https://i.imgur.com/ULvqsvt.png)
+
+You can then view the website in your browser at `https://localhost`
+
+![](https://i.imgur.com/4AeJ9Rs.png)
+
+Your browser may warn you the certificate is self signed, this is normal.
+
+If you changed the port you have to use `https://localhost:PORT/`
+
+### Default Structure
+
+This structure is designed to keep the project organized and maintainable, separating error handling, website content, and server configuration.
+
+```
+/root
+│
+├── /error
+│   ├── 404.html
+│   └── 500.html
+│
+├── /website
+│   └── index.html
+│
+└── server-ssl.js
+```
+
+### 404/500 Pages
+
+The server is configured to serve custom `404` and `500` error pages. 
+
+![](https://i.imgur.com/LvLnXMR.png)
+
+Currently everything is treated like a `Server Error` except for `Page Not Found`
+
+![](https://i.imgur.com/HJoNquS.png)
+
+If you want to add more specific custom error pages, place them in the `/error` folder and update `server-ssl.js`
+
+### Port
+
+By default, the server runs on port `3000`
+
+You can provide a different port as an argument.
+
+```
+node server-ssl.js --port=443 --cert="certificate.pem" --pk="private-key.pem"
+```
+
+You can also set the `PORT` env variable in `Node.js` or modify `server-ssl.js`
---
+++ server-ssl.js
@@ -0,0 +1,146 @@
+import { createServer } from 'https';
+import { readFile, readFileSync, existsSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { join, extname as _extname, dirname } from 'path';
+
+// openssl req -x509 -newkey rsa:2048 -nodes -sha256 -keyout private-key.pem -out certificate.pem -days 365 -subj "//CN=localhost"
+// node server-ssl.js port=443
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const __args = process.argv.slice(2);
+const options = {};
+
+let optPk = null;
+let optCert = null;
+let optRoot = null;
+let optError = null;
+let optPort = process.env.PORT || 3000;
+
+loadArguments();
+
+!existsSync(optPk) && certificateNotExist();
+!existsSync(optCert) && certificateNotExist();
+
+options.key = readFileSync(optPk);
+options.cert = readFileSync(optCert);
+
+/**
+ * Creates an HTTPS server that handles incoming requests.
+ * 
+ * @function
+ * @param {Request} req - The incoming request object.
+ * @param {Response} res - The response object to send data back to the client.
+ */
+const server = createServer(options, (req, res) => {
+    let filePath = join(__dirname, optRoot, req.url === '/' ? 'index.html' : req.url);
+
+    const extname = _extname(filePath);
+
+    let contentType;
+
+    switch (extname) {
+        case '.html':
+            contentType = 'text/html';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.gif':
+            contentType = 'image/gif';
+            break;
+        case '.svg':
+            contentType = 'image/svg+xml';
+            break;
+        case '.ico':
+            contentType = 'image/x-icon';
+            break;
+    }
+
+    readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                readFile(join(__dirname, '/error/404.html'), (err404, content404) => {
+                    if (err404) {
+                        res.writeHead(500);
+                        res.end('Server Error');
+                    } else {
+                        res.writeHead(404, { 'Content-Type': 'text/html' });
+                        res.end(content404);
+                    }
+                });
+            } else {
+                readFile(join(__dirname, '/error/500.html'), (error500, content500) => {
+                    if (error500) {
+                        res.writeHead(500);
+                        res.end('Server Error');
+                    } else {
+                        res.writeHead(500, { 'Content-Type': 'text/html' });
+                        res.end(content500);
+                    }
+                });
+            }
+            return;
+        }
+
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(content);
+    });
+});
+
+server.listen(optPort, (err) => {
+    if (err) {
+        console.error('Error starting server:', err);
+        return;
+    }
+    console.log(`Server is running on https://localhost:${optPort}`);
+});
+
+function certificateNotExist() {
+    console.log("You need to generate or provide an SSL Certificate and Private Key in PEM format");
+    console.log("You can use the following command from git bash");
+    console.log(" ");
+    console.log('openssl req -x509 -newkey rsa:2048 -nodes -sha256 -keyout private-key.pem -out certificate.pem -days 365 -subj "//CN=localhost"');
+    process.exit(1);
+}
+
+function loadArguments() {
+    __args.forEach((e) => {
+        let portArg = e.toLowerCase().includes("--port=") ? e.split("=")[1] : null
+        if (portArg !== null) {
+            optPort = portArg;
+        }
+
+        let certPath = e.toLowerCase().includes("--cert=") ? e.split("=")[1] : null
+        if (certPath !== null) {
+            optCert = certPath;
+        }
+
+        let privateKeyPath = e.toLowerCase().includes("--pk=") ? e.split("=")[1] : null
+        if (privateKeyPath !== null) {
+            optPk = privateKeyPath;
+        }
+
+        let rootFolder = e.toLowerCase().includes("--root=") ? e.split("=")[1] : null
+        if (rootFolder !== null) {
+            optRoot = rootFolder;
+        }
+    });
+
+    !optPk && (optPk = 'private-key.pem');
+    !optCert && (optCert = 'certificate.pem');
+    !optRoot && (optRoot = 'website');
+    !optError && (optError = 'error');
+}
