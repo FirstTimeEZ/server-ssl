21a4cf877bc444964a56fc0c30c6cff6c9665cd0
FirstTimeEZ <179362031+FirstTimeEZ@users.noreply.github.com>
2024-11-22 22:35:44 +1300

    lets encrypt!
    This is a WIP and does not currently generate certificates

--- ssl/module/lets-encrypt-http.js
+++ ssl/module/lets-encrypt-http.js
@@ -1,4 +1,5 @@
 import * as jose from './jose/index.js';
+import { writeFile, readFileSync, existsSync, mkdirSync } from 'fs';
 
 // This isn't finished and does not generate certificates
 // Anything might change until its finished
@@ -15,6 +16,8 @@ import * as jose from './jose/index.js';
 const DIRECTORY_URL = "https://acme-staging-v02.api.letsencrypt.org/directory";
 
 const ALG_ECDSA = 'ES256';
+const PUBLIC_KEY = '/publicKey.raw';
+const PRIVATE_KEY = '/privateKey.raw';
 
 const CONTENT_TYPE_JOSE = 'application/jose+json';
 
@@ -136,17 +139,58 @@ export async function createOrder(kid, nonce, keyPair, newOrderUrl, identifiers)
 }
 
 /**
- * Generates a key pair, The generated public key will be associated with a user account.
- * The generated private key will be used to sign requests.
+ * Generates a public-private key pair for cryptographic operations.
+ * The generated public key will be associated with a user account,
+ * while the private key will be used to sign requests.
+ *
+ * If existing keys are found at the specified path, they will be loaded
+ * from the filesystem instead of generating new ones.
+ *
+ * @param {string} sslPath - The file path where the public and private keys are stored.
+ *                            The keys will be saved as 'publicKey.raw' and 'privateKey.raw'
+ *                            if they do not already exist.
  *
  * @returns {Promise<{ publicKey: CryptoKey, privateKey: CryptoKey }>} 
- *          An object containing the generated public and private keys.
- *          The public key is linked to the user account.
+ *          An object containing the generated or loaded public and private keys.
+ *          The public key is linked to the user account and can be shared,
+ *          while the private key must be kept secure and confidential.
  *
+ * @throws {Error} Throws an error if key generation or file operations fail.
+ *
+ * @example
+ * const keyPair = await generateKeyPair('/path/to/keys/'); 
  */
-export async function generateKeyPair() {
-    const { publicKey, privateKey } = await jose.generateKeyPair(ALG_ECDSA, { extractable: true });
-    return { publicKey, privateKey };
+export async function generateKeyPair(sslPath) {
+    let keys = {};
+
+    if (existsSync(sslPath + PUBLIC_KEY) && existsSync(sslPath + PRIVATE_KEY)) {
+        const publicKeyRaw = readFileSync(sslPath + PUBLIC_KEY);
+        const privateKeyRaw = readFileSync(sslPath + PRIVATE_KEY);
+
+        keys.publicKey = await jose.importSPKI(publicKeyRaw.toString(), ALG_ECDSA, { extractable: true });
+        keys.privateKey = await jose.importPKCS8(privateKeyRaw.toString(), ALG_ECDSA, { extractable: true });
+
+        console.log("Load Keys From File", keys);
+    }
+    else {
+        const { publicKey, privateKey } = await jose.generateKeyPair(ALG_ECDSA, { extractable: true });
+        keys.publicKey = publicKey;
+        keys.privateKey = privateKey;
+
+        const publicKeyRaw = await jose.exportSPKI(publicKey);
+        const privateKeyRaw = await jose.exportPKCS8(privateKey);
+
+        console.log(publicKeyRaw, privateKeyRaw);
+
+        mkdirSync(sslPath, { recursive: true });
+
+        writeFile(sslPath + PUBLIC_KEY, publicKeyRaw, () => { });
+        writeFile(sslPath + PRIVATE_KEY, privateKeyRaw, () => { });
+
+        console.log('Raw ES256 keys saved to publicKey.raw and privateKey.raw');
+    }
+
+    return keys;
 }
 
 /**
@@ -177,21 +221,20 @@ export async function signPayloadJson(payload, protectedHeader, keyPair) {
  * If no keyPair is provided, a random one will be generated and used instead.
  *
  * @param {string} fqdn - The fully qualified domain name (FQDN) for which to manage SSL certificates.
- * @param {Object} [optionalKeyPair] - An optional keypair for authentication/account.
- * @param {string} optionalKeyPair.publicKey - The public key part of the keypair.
- * @param {string} optionalKeyPair.privateKey - The private key part of the keypair.
+ * @param {string} sslPath - The file path where the public and private keys are stored.
+ *                            The keys will be saved as 'publicKey.raw' and 'privateKey.raw'
+ *                            if they do not already exist.
+ *
  * @see {generateKeyPair}
  */
-export async function startLetsEncryptDaemon(fqdn, optionalKeyPair) {
-    if (optionalKeyPair == undefined) {
-        optionalKeyPair = await generateKeyPair();
-    }
+export async function startLetsEncryptDaemon(fqdn, optionalSslPath) {
+    const keyPair = await generateKeyPair(optionalSslPath);
 
-    if (optionalKeyPair.publicKey == undefined) {
+    if (keyPair.publicKey == undefined) {
         throw new Error("optionalKeyPair.publicKey must be defined");
     }
 
-    if (optionalKeyPair.privateKey == undefined) {
+    if (keyPair.privateKey == undefined) {
         throw new Error("optionalKeyPair.publicKey must be defined");
     }
 
@@ -206,13 +249,13 @@ export async function startLetsEncryptDaemon(fqdn, optionalKeyPair) {
         const nonce = await newNonceAsync(directory.newNonce);
 
         if (nonce.nonce !== null) {
-            const account = await createAccount(nonce.nonce, directory.newAccount, optionalKeyPair).catch(console.error);
+            const account = await createAccount(nonce.nonce, directory.newAccount, keyPair).catch(console.error);
 
             if (account.answer.account && account.answer.account.status == "valid") {
                 console.log("Account Created and Valid", account.answer);
                 console.log("Next Nonce", account.nonce);
 
-                const order = await createOrder(account.answer.location, account.nonce, optionalKeyPair, directory.newOrder, [{ "type": "dns", "value": fqdn }]);
+                const order = await createOrder(account.answer.location, account.nonce, keyPair, directory.newOrder, [{ "type": "dns", "value": fqdn }]);
                 if (order.answer.order != undefined) {
                     console.log(order.answer.order);
                     console.log("Next Nonce", order.nonce);
@@ -234,69 +277,30 @@ export async function startLetsEncryptDaemon(fqdn, optionalKeyPair) {
     }
 }
 
-// {
-//     '5JzAlUrt3Pk': 'https://community.letsencrypt.org/t/adding-random-entries-to-the-directory/33417',
-//     keyChange: 'https://acme-staging-v02.api.letsencrypt.org/acme/key-change',
-//     meta: {
-//       caaIdentities: [ 'letsencrypt.org' ],
-//       termsOfService: 'https://letsencrypt.org/documents/LE-SA-v1.4-April-3-2024.pdf',
-//       website: 'https://letsencrypt.org/docs/staging-environment/'
-//     },
-//     newAccount: 'https://acme-staging-v02.api.letsencrypt.org/acme/new-acct',
-//     newNonce: 'https://acme-staging-v02.api.letsencrypt.org/acme/new-nonce',
-//     newOrder: 'https://acme-staging-v02.api.letsencrypt.org/acme/new-order',
-//     renewalInfo: 'https://acme-staging-v02.api.letsencrypt.org/draft-ietf-acme-ari-03/renewalInfo',
-//     revokeCert: 'https://acme-staging-v02.api.letsencrypt.org/acme/revoke-cert'
-//   }
-//   Account Created and Valid {
-//     account: {
-//       key: {
-//         kty: 'EC',
-//         crv: 'P-256',
-//         x: '[removed personal info]',
-//         y: '[removed personal info]'
-//       },
-//       initialIp: '[removed personal info]',
-//       createdAt: '2024-11-22T08:15:06.116330285Z',
-//       status: 'valid'
-//     },
-//     location: 'https://acme-staging-v02.api.letsencrypt.org/acme/acct/172696254'
-//   }
-//   Next Nonce jDZhkUVkLGJl0GHzQYG_t6IMRqtfhawfJT3Nr8VeRXowCuBSxFU
-//   {
-//     status: 'pending',
-//     expires: '2024-11-29T08:15:06Z',
-//     identifiers: [ { type: 'dns', value: 'www.thisismydomainhowdoyouknow123.org' } ],
-//     authorizations: [
-//       'https://acme-staging-v02.api.letsencrypt.org/acme/authz/172696254/15031989944'
-//     ],
-//     finalize: 'https://acme-staging-v02.api.letsencrypt.org/acme/finalize/172696254/20696202154'
-//   }
-// authorizations {
-//     "identifier": {
-//       "type": "dns",
-//       "value": "www.thisismydomainhowdoyouknow123.org"
-//     },
-//     "status": "pending",
-//     "expires": "2024-11-29T08:15:06Z",
-//     "challenges": [
-//       {
-//         "type": "http-01",
-//         "url": "https://acme-staging-v02.api.letsencrypt.org/acme/chall/172696254/15031989944/S__T2A",
-//         "status": "pending",
-//         "token": "nhUcxf4_n5JPnCD2KVVD7DzSHV37FmZxWnGBNap8XKQ"
-//       },
-//       {
-//         "type": "tls-alpn-01",
-//         "url": "https://acme-staging-v02.api.letsencrypt.org/acme/chall/172696254/15031989944/_TPVyA",
-//         "status": "pending",
-//         "token": "nhUcxf4_n5JPnCD2KVVD7DzSHV37FmZxWnGBNap8XKQ"
-//       },
-//       {
-//         "type": "dns-01",
-//         "url": "https://acme-staging-v02.api.letsencrypt.org/acme/chall/172696254/15031989944/Zk2vUg",
-//         "status": "pending",
-//         "token": "nhUcxf4_n5JPnCD2KVVD7DzSHV37FmZxWnGBNap8XKQ"
-//       }
-//     ]
-//   }
+// +-------------------+--------------------------------+--------------+
+// | Action            | Request                        | Response     |
+// +-------------------+--------------------------------+--------------+
+// | Get directory     | GET  directory                 | 200          | x
+// |                   |                                |              |
+// | Get nonce         | HEAD newNonce                  | 200          | x
+// |                   |                                |              |
+// | Create account    | POST newAccount                | 201 ->       | x
+// |                   |                                | account      |
+// |                   |                                |              |
+// | Submit order      | POST newOrder                  | 201 -> order | x
+// |                   |                                |              |
+// | Fetch challenges  | POST-as-GET order's            | 200          |
+// |                   | authorization urls             |              |
+// |                   |                                |              |
+// | Respond to        | POST authorization challenge   | 200          |
+// | challenges        | urls                           |              |
+// |                   |                                |              |
+// | Poll for status   | POST-as-GET order              | 200          |
+// |                   |                                |              |
+// | Finalize order    | POST order's finalize url      | 200          |
+// |                   |                                |              |
+// | Poll for status   | POST-as-GET order              | 200          |
+// |                   |                                |              |
+// | Download          | POST-as-GET order's            | 200          |
+// | certificate       | certificate url                |              |
+// +-------------------+--------------------------------+--------------+
