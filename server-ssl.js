58e394ae8b42a8e0243be0484775c16026bb57f3
FirstTimeEZ <179362031+FirstTimeEZ@users.noreply.github.com>
2024-11-26 22:36:36 +1300

    server-ssl.js
    Display exact time until certificate expires, eg.
    
    ```
    J@BUD MINGW64 ~/Desktop/server-ssl (main)
    $ ./start-windows.bat --letsEncrypt --domains=["www.ssl.boats","ssl.boats"] --staging --autoRestart
    Starting SSL Web Server
    Node.js already exists
    --------
    Auto Restart Enabled
    Server will restart after certificates are renewed
    --------
    Time until renewal required: 89 days, 22 hours, 48 minutes, 35 seconds
    Starting Lets Encrypt ACME Daemon!
    Copyright © 2024 FirstTimeEZ
    --------
    It has been: 770.028 seconds since you last generated certificates
    HTTPS Server is running on port 443
    HTTP Server is redirecting requests to 443
    ```
    
    /root
    │
    ├── /error
    │   ├── 404.html
    │   └── 500.html
    │
    ├── /ssl
    │   ├── /module
    │   │   ├── /crypt
    │   │   ├── /jose
    │   │   └── lets-encrypt-acme-client.js
    │   │
    │   ├── acmeKeys
    │   ├── private-key.pem
    │   └── certificate.pem
    │
    ├── /website
    │   └── index.html <---- Your website goes here
    │
    ├── node.exe
    ├── server-ssl.js
    └── start-windows.bat

--- server-ssl.js
+++ server-ssl.js
@@ -1,99 +1,10 @@
-/**
- * Copyright © 2024 FirstTimeEZ
- * https://github.com/FirstTimeEZ
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import { fileURLToPath } from 'url';
 import { createServer as createServerHTTPS } from 'https';
 import { createServer as createServerHTTP } from 'http';
-import { readFile, readFileSync, existsSync, mkdir } from 'fs';
+import { readFile, readFileSync, existsSync } from 'fs';
 import { join, extname as _extname, dirname } from 'path';
 import { startLetsEncryptDaemon, checkChallengesMixin } from './ssl/module/lets-encrypt-acme-client.js'
-
-const S_SSL = {
-    // Config
-    override: false,
-    urlsArray: null,
-    autoRestartAvailable: null,
-    // Args
-    optPk: null,
-    optCert: null,
-    optError: null,
-    optEntry: null,
-    optStaging: null,
-    optWebsite: null,
-    optDomains: null,
-    optLetsEncrypt: null,
-    optAutoRestart: null,
-    optGenerateAnyway: null,
-    optPort: process.env.PORT || 443,
-    optPortHttp: process.env.PORT_HTTP || 80,
-    // Methods
-    certNotExist: () => {
-        console.log(" ");
-        console.log("Certificate and Private Key not found or don't exist, they should be in the ssl folder");
-        console.log(" ");
-        console.log("You need to generate or provide an SSL Certificate and Private Key in PEM format");
-        console.log("You can use the following command from git bash or run start-windows.bat with no arguments");
-        console.log(" ");
-        console.log('openssl req -x509 -newkey rsa:2048 -nodes -sha256 -keyout ssl/private-key.pem -out ssl/certificate.pem -days 365 -subj "//CN=localhost"');
-        process.exit(1);
-    },
-    useSslFolder: (sslFolder) => {
-        mkdir(sslFolder, { recursive: true }, () => { });
-        console.log(" ");
-        console.log("Put your Certificate and Private Key in the SSL folder that was just created");
-        console.log(" ");
-        process.exit(1);
-    },
-    loadErrorPages: (__errorDir) => {
-        readFile(join(__errorDir, '/404.html'), (err404, content404) => !err404 && (S_SSL.ERROR_404_PAGE = content404));
-        readFile(join(__errorDir, '/500.html'), (err500, content500) => !err500 && (S_SSL.ERROR_500_PAGE = content500));
-    },
-    timeUntilRenew: (notAfter) => {
-        const specificDate = new Date(notAfter);
-        const currentDate = new Date();
-
-        const timeDifference = specificDate.getTime() - currentDate.getTime();
-        const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        const hoursDifference = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutesDifference = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        const secondsDifference = Math.floor((timeDifference % (1000 * 60)) / 1000);
-
-        console.log(`Time until renewal required: ${daysDifference} days, ${hoursDifference} hours, ${minutesDifference} minutes, ${secondsDifference} seconds`);
-    },
-    // Pages
-    ERROR_404_PAGE: null,
-    ERROR_500_PAGE: null,
-    // Consts
-    SUCCESS: 200,
-    REDIRECT: 301,
-    ONE_DAY_MILLISECONDS: 86400000,
-    PAGE_NOT_FOUND: 'ENOENT',
-    ERROR_NOT_FOUND: '404 - File Not Found',
-    ERROR_SERVER: '500 - Server Error',
-    ERROR_STARTING: 'Error starting server',
-    STARTED_HTTPS: 'HTTPS Server is running on port ',
-    STARTED_HTTP: 'HTTP Server is redirecting requests to ',
-    WEBSITE_ROOT: '/',
-    SSL: "ssl",
-    TEXT_HTML: 'text/html',
-    CONTENT_TYPE: 'Content-Type',
-    HTTPS: 'https://',
-    REDIRECT_LOCATION: 'Location',
-}
+import { S_SSL, importRequiredArguments } from './ssl.js'
 
 const CONTENT_TYPES = {
     '.css': 'text/css',
@@ -107,7 +18,7 @@ const CONTENT_TYPES = {
 };
 
 try {
-    loadArguments();
+    importRequiredArguments();
 
     const __rootDir = dirname(fileURLToPath(import.meta.url));
     const __websiteDir = join(__rootDir, S_SSL.optWebsite);
@@ -151,38 +62,4 @@ try {
     /////////////////////////////////////////////////////////////////
 } catch (exception) {
     console.error(exception);
-}
-
-function loadArguments() {
-    process.argv.slice(2).forEach((arg) => {
-        let rightSide = arg.split("=")[1];
-        arg.includes("--port=") && (S_SSL.optPort = rightSide);
-        arg.includes("--portHttp=") && (S_SSL.optPortHttp = rightSide);
-        arg.includes("--cert=") && (S_SSL.optCert = rightSide);
-        arg.includes("--pk=") && (S_SSL.optPk = rightSide);
-        arg.includes("--site=") && (S_SSL.optWebsite = rightSide);
-        arg.includes("--error=") && (S_SSL.optError = rightSide);
-        arg.includes("--entry=") && (S_SSL.optEntry = rightSide);
-        arg.includes("--notAfter=") && (S_SSL.expireDate = rightSide);
-
-        arg.includes("--letsEncrypt") && (S_SSL.optLetsEncrypt = true);
-        arg.includes("--domains") && (S_SSL.optDomains = rightSide);
-        arg.includes("--generateAnyway") && (S_SSL.optGenerateAnyway = true);
-        arg.includes("--staging") && (S_SSL.optStaging = true);
-        arg.includes("--autoRestart") && (S_SSL.optAutoRestart = true);
-        arg.includes("--arAvailable") && (S_SSL.autoRestartAvailable = true);
-        arg.includes("--ok") && (S_SSL.override = true);
-    });
-
-    S_SSL.autoRestartAvailable == false && S_SSL.override === false && (console.log("--------"), console.log("Server must be started with start-windows.bat to enable auto restart"), console.log("If you have a way to restart the server on error code 123, use override --ok"), console.log("--------"), S_SSL.optAutoRestart = false);
-    S_SSL.optAutoRestart === true && (console.log("--------"), console.log("Auto Restart Enabled"), console.log("Server will restart after certificates are renewed"), console.log("--------"))
-    S_SSL.optLetsEncrypt && S_SSL.optDomains === null && (console.log("You must specify at least one domain to use --letsEncrypt"), S_SSL.optLetsEncrypt = false);
-
-    !S_SSL.optPk && (S_SSL.optPk = 'private-key.pem');
-    !S_SSL.optCert && (S_SSL.optCert = 'certificate.pem');
-    !S_SSL.optWebsite && (S_SSL.optWebsite = 'website');
-    !S_SSL.optError && (S_SSL.optError = 'error');
-    !S_SSL.optEntry && (S_SSL.optEntry = 'index.html');
-
-    S_SSL.expireDate && S_SSL.timeUntilRenew(S_SSL.expireDate);
 }
