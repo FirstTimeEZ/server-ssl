385fe0d5a341e61eeddb0cdae09b5592acfa12ac
FirstTimeEZ <179362031+FirstTimeEZ@users.noreply.github.com>
2024-11-29 04:57:51 +1300

    server-ssl.js (#10)
    Moved http redirect into S_SSL
    
    /root
    │
    ├── /error
    │   ├── 404.html
    │   └── 500.html
    │
    ├── /ssl
    │   ├── /openssl
    │   ├── /module
    │   │   ├── /crypt
    │   │   ├── /jose
    │   │   └── lets-encrypt-acme-client.js
    │   │
    │   ├── acmeKeys
    │   ├── private-key.pem
    │   └── certificate.pem
    │   └── ssl.js
    │
    ├── /website
    │   └── index.html <---- Your website goes here
    │
    ├── node.exe
    ├── server-ssl.js
    └── start-windows.bat

--- server-ssl.js
+++ server-ssl.js
@@ -32,18 +32,11 @@ const HTTPS_SERVER = createServerHTTPS({ key: readFileSync(__pkPath), cert: read
     readFile(filePath, (err, content) => !err ? (res.writeHead(S_SSL.SUCCESS, { [S_SSL.CONTENT_TYPE]: contentType }), res.end(content)) : S_SSL.getErrorPage(res, err));
 }).on('error', (e) => e.code === S_SSL.ADDR_IN_USE && console.error(`${S_SSL.optPort}${S_SSL.IN_USE}`)).listen(S_SSL.optPort, (err) => err ? console.error(S_SSL.ERROR_STARTING, err) : console.log(`${S_SSL.STARTED_HTTPS}${S_SSL.optPort}`));
 
-createServerHTTP((req, res) => {
-    if (S_SSL.optLetsEncrypt) { if (checkChallengesMixin(req, res)) { return; } } // Lets Encrypt! HTTP-01 ACME Challenge Mixin
-    res.writeHead(S_SSL.REDIRECT, { [S_SSL.REDIRECT_LOCATION]: `${S_SSL.HTTPS}${req.headers.host}${req.url}` }); // Always Redirect HTTP to HTTPS unless doing a ACME Challenge
-    res.end();
-}).on('error', (e) => e.code === S_SSL.ADDR_IN_USE && console.error(`${S_SSL.optPortHttp}${S_SSL.IN_USE}`)).listen(S_SSL.optPortHttp, () => console.log(`${S_SSL.STARTED_HTTP}${S_SSL.optPort}`));
+createServerHTTP((req, res) => S_SSL.optLetsEncrypt ? !checkChallengesMixin(req, res) && S_SSL.redirect(res, req) : S_SSL.redirect(res, req)).on('error', (e) => e.code === S_SSL.ADDR_IN_USE && console.error(`${S_SSL.optPortHttp}${S_SSL.IN_USE}`))
+    .listen(S_SSL.optPortHttp, () => console.log(`${S_SSL.STARTED_HTTP}${S_SSL.optPort}`)); // Lets Encrypt! HTTP-01 ACME Challenge Mixin - Always Redirect HTTP to HTTPS unless doing a ACME Challenge
 
 S_SSL.loadLetsEncryptDaemon(__sslFolder, // Lets Encrypt! ACME Daemon
-    () => { // Restart Callback, use --autoRestart
-        console.log("Restarting Soon");
-    }, 30, // Restart Seconds (number of callbacks)
-    () => { // Update Certificates Callback
-        HTTPS_SERVER.setSecureContext({ key: readFileSync(__pkPath), cert: readFileSync(__certPath) });
-        console.log("Updated Server Certificate");
-    });
+    () => console.log("Restarting Soon"), 30, // seconds (also # of callbacks)
+    () => (HTTPS_SERVER.setSecureContext({ key: readFileSync(__pkPath), cert: readFileSync(__certPath) }), console.log("Updated Server Certificate"))); // Update Certificates Callback
+
 S_SSL.checkNodeForUpdates(__sslFolder); // Check Node.js version
