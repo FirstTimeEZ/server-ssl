bb50c52b3cb0c870e141f4b01963dba0e9ed8e09
FirstTimeEZ <179362031+FirstTimeEZ@users.noreply.github.com>
2024-12-02 15:28:23 +1300

    server-ssl.js
    add S_SSL.respondWithContent
    add S_SSL.respondWithNotFound
    add S_SSL.respondWithServerError
    
    Adds some helpers so you can use to easily respond to api requests
    
    keep in mind you can remove everything inside HTTPS_SERVER and do whatever you like, this doesn't have to be a "web server"
    
    fixes a bug that meant error pages didn't have the correct error code
    
    /root
    │
    ├── /error
    │   ├── 404.html
    │   └── 500.html
    │
    ├── /ssl
    │   ├── /openssl
    │   ├── /module
    │   │   ├── /crypt
    │   │   ├── /jose
    │   │   └── lets-encrypt-acme-client.js
    │   │
    │   ├── acmeKeys
    │   ├── private-key.pem
    │   └── certificate.pem
    │   └── ssl.js
    │
    ├── /website
    │   └── index.html <---- Your website goes here
    │
    ├── node.exe
    ├── server-ssl.js            <--- server config
    └── start-windows.bat

--- website/index.js
+++ website/index.js
@@ -3,17 +3,23 @@ setTimeout(() => {
         const testLoadImage = new Image();
         testLoadImage.src = "bg.png";
 
-        document.getElementById("spinner").hidden = true;
-
-        if (testLoadImage.complete) {
+        testLoadImage.onload = function () {
+            document.getElementById("spinner").hidden = true;
             document.getElementById("started").hidden = false;
             document.getElementById("started-text").hidden = false;
             console.log("Server Started Successfully");
-            return;
-        }
-    } catch { }
+        };
 
-    document.getElementById("failed").hidden = false;
-    document.getElementById("failed-text").hidden = false;
-    console.log("Failed to load test image, something is wrong");
+        testLoadImage.onerror = function () {
+            document.getElementById("spinner").hidden = true;
+            document.getElementById("failed").hidden = false;
+            document.getElementById("failed-text").hidden = false;
+            console.log("Failed to load test image, something is wrong");
+        };
+    } catch {
+        document.getElementById("spinner").hidden = true;
+        document.getElementById("failed").hidden = false;
+        document.getElementById("failed-text").hidden = false;
+        console.log("Failed to load test image, something is wrong");
+    }
 }, 2000);
