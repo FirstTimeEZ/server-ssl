ca26b3bf00ef6dbebe2d112ea79f13b46c314e0f
FirstTimeEZ <179362031+FirstTimeEZ@users.noreply.github.com>
2024-12-05 01:07:07 +1300

    server-ssl.js
    /root
    │
    ├── /error
    │   ├── 404.html
    │   └── 500.html
    │
    ├── /ssl
    │   ├── /openssl
    │   ├── /module
    │   │   ├── /crypt
    │   │   ├── /jose
    │   │   └── lets-encrypt-acme-client.js
    │   │
    │   ├── acmeKeys
    │   ├── private-key.pem
    │   └── certificate.pem
    │   └── ssl.js
    │
    ├── /website
    │   └── index.html <---- Your website goes here
    │
    ├── node.exe
    ├── server-ssl.js            <--- server config
    └── start-windows.bat

--- website/index.js
+++ website/index.js
@@ -3,7 +3,7 @@ setTimeout(() => {
         fetch("/api/time").then((response) => {
             response.json().then((time) => {
                 if (time != undefined && time != "") {
-                    document.getElementById("time").innerText = new Date(time).toLocaleTimeString() + " - " + new Date(time).toDateString();
+                    document.getElementById("time").innerText = new Date(time).toTimeString() + "\n" + new Date(time).toDateString();
                     document.getElementById("time").hidden = false;
                     document.getElementById("spinner").hidden = true;
                 }
